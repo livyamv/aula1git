32b55bc0bb24aedaa65e9276e0add205f0c0f219
livia reis <159190559+livyamv@users.noreply.github.com>
2024-06-12 10:50:06 -0300

    Add files via upload

---
+++ Funcoes12-06.py
@@ -0,0 +1,16 @@
+import time
+
+def hi_everybody (lista):
+    for name in lista:
+        print ("Oi, name")
+
+hi_everybody (["Tina","Gabi", "Jão" ])
+
+
+def criar_lista(n):
+    lista = []
+    for i in range (n):
+        lista.append(i)
+        return lista
+print (criar_lista(10))
+time.sleep(3)
---
+++ Tuplas_Dic.py
@@ -0,0 +1,42 @@
+import time
+
+#tupla = tuple ()
+
+tupla = (1)
+
+tupla = (1,)
+
+tupla = 1,2,3
+
+print (tupla)
+
+print (tupla[1])
+
+#tupla[0] = 100 #Erro, pois não é possivel alterar uma tupla
+
+#Manipulando dicionários :
+dic = {"semMundial": "Palmeiras", "1mundial": "Corinthians", "2mundial": "São Paulo"}
+
+print(dic["semMundial"])
+
+notas = {"mat": 5, "lp": 10,"ef":8}
+
+print (notas)
+print (notas["lp"])
+
+
+#print (notas["bio"])
+
+print (dir(notas))
+
+print(notas.values())
+
+print(notas.keys())
+
+print(len(notas))
+
+
+for disciplina in notas.keys():
+    print(disciplina)
+
+time.sleep(3)
